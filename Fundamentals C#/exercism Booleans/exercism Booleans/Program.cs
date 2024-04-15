46ce97f0932824281f0dfbdb0cc6cde0e464275f
sandraY <sandra@yakha.eu>
2024-04-15 21:07:17 +0200

    firstcommit

---
+++ Fundamentals C#/exercism Booleans/exercism Booleans/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake)
+    {
+        if (knightIsAwake == false)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFastAttack() method");
+    }
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+    {
+        if (knightIsAwake || archerIsAwake || prisonerIsAwake)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSpy() method");
+    }
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+    {
+        if (archerIsAwake == false && prisonerIsAwake == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSignalPrisoner() method");
+    }
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        if ((archerIsAwake == false && petDogIsPresent == true) || (petDogIsPresent == false && knightIsAwake == false && archerIsAwake == false && prisonerIsAwake == true))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFreePrisoner() method");
+    }
+}
---
+++ Fundamentals C#/exercism Strings/exercism Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+static class LogLine
+{
+    public static string Message(string logLine)
+    {
+        int i = logLine.IndexOf(':') + 1;
+        string result = logLine.Substring(i).Trim();
+        return result;
+    }
+
+    public static string LogLevel(string logLine)
+    {
+        int a = logLine.IndexOf(':') - 2;
+        string result = logLine.Substring(1, a).ToLower();
+        return result;
+
+    }
+
+    public static string Reformat(string logLine)
+    {
+        return $"{Message(logLine)} ({LogLevel(logLine)})";
+    }
+}
